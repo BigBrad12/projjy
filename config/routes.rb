b19ff773fa5f83657553ecfc5d35a0d4247d8632
Bradley Slater <brad@bradhost.bradpc>
2023-03-26 03:55:48 +0200

    routes setup and front end basic

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,30 @@
 Rails.application.routes.draw do
+  
+  # authentication routes
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  
+  # home root
   root "home#index"
+
+  # adding viewing and editing teams within a company, adding viewing and editing users within a company
+  resources :companies do
+    resources :teams, only: [:show, :edit, :create]
+    resources :users, only: [:show, :edit, :create]
+  end
+
+  # adding users to teams and projects to teams
+  resources :teams do
+    resources :users, only: [:index, :new, :create]
+    resources :team_projects, only: [:index, :new, :create, :destroy]
+  end
+
+  #  users can view projects
+  resources :users do
+    resources :projects, only: [:index]
+  end
+
+  # adding viewing and removing users to projects
+  resources :projects do 
+    resources :project_users, only: [:index, :new, :create, :destroy]
+  end
 end
